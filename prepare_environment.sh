f675b5e69b66869808a89a09f58c8e6227289768
Mustafa Ozcelikors <mozcelikors@gmail.com>
2023-10-08 01:02:01 +0300

    Fix issues with the environment preparation script
    Signed-off-by: Mustafa Ozcelikors <mozcelikors@gmail.com>

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -13,14 +13,17 @@ SCRIPTDIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
 sudo steamos-readonly disable
 sudo steamos-readonly status
+# We delete keys because after SteamOS update, pacman-key --init does not work.
+sudo rm -rf /etc/pacman.d/gnupg
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-sudo pacman -S --overwrite \* fakeroot
-sudo pacman -Sy --needed base-devel
-sudo pacman -Sy --needed archlinux-keyring autoconf automake binutils bison debugedit fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed sudo which glibc hwinfo linux-api-headers qt5-base cmake jsoncpp libuv rhash glibc linux-neptune-headers
-sudo pacman -Sy --needed qt6-base qt6-5compat qt6-declarative qt6-multimedia qt6ct qt6-shadertools
-sudo pacman -Sy --needed mesa glu mesa-unstable mesa-utils mesa-vdpau lib32-mesa lib32-mesa-vdpau lib32-libglvnd libglvnd
-sudo pacman -Syu glibc linux-api-headers
+sudo pacman -S --noconfirm --overwrite \* fakeroot
+sudo pacman -Sy --noconfirm base-devel
+sudo pacman -Sy --noconfirm archlinux-keyring autoconf automake binutils bison debugedit fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed sudo which glibc hwinfo linux-api-headers qt5-base cmake jsoncpp libuv rhash glibc linux-neptune-headers
+sudo pacman -Sy --noconfirm qt6-base qt6-5compat qt6-declarative qt6-multimedia qt6ct qt6-shadertools
+sudo pacman -Sy --noconfirm mesa glu mesa-unstable mesa-utils mesa-vdpau lib32-mesa lib32-mesa-vdpau lib32-libglvnd libglvnd
+sudo pacman -Sy --noconfirm lib32-mesa lib32-mesa-vdpau lib32-libglvnd libglvnd
+sudo pacman -Syu --noconfirm glibc linux-api-headers
 sudo steamos-readonly enable
 
 ln -s $SCRIPTDIR/BetterGallery.desktop /home/deck/Desktop
